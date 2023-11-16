527ec5ff70c7f55479b1f0d582e1ce3308c106a7
Elham Mey <elhammey@gamil.com>
2023-11-16 07:45:21 +0330

    change core setting

--- digital_store/urls.py
+++ digital_store/urls.py
@@ -19,11 +19,30 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
+    # Admin
     path("admin/", admin.site.urls),
-    path("", include("products.urls")),
-    path("", include("users.urls")),
+
+    # Product
+    path("product/", include("products.urls")),
+
+    # User
+    path("user/", include("users.urls")),
+
+    # Sub
+    path('subs/', include("subscriptions.urls")),
+
+    # Payment
+    path('payments/', include('payments.urls')),
+
+    # Token simple jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 if settings.IS_DEVEL:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
